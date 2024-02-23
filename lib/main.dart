1c3d125bc6faf984f0123edc784178df1d3e0ddf
Samuel <samuel9lmdv@gmail.com>
2024-02-23 17:32:01 +0100

    Added CircularProgressIndicator
    Added CircularProgressIndicator to wait dices. Quick fix blue alerts

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,173 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        title: Text('Dicee'),
+    debugShowCheckedModeBanner: false,
+    home: DefaultTabController(
+      length: 2,
+      child: Scaffold(
         backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text(
+            'Dicee',
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        bottomNavigationBar: TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white70,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorPadding: const EdgeInsets.all(5.0),
+            indicatorColor: Colors.redAccent,
+            tabs: [
+              Tab(
+                icon: Image.asset('images/icons8-dice-100.png'),
+              ),
+              Tab(
+                icon: Image.asset('images/icons8-cubos-de-dados-64.png'),
+              )
+            ]),
+        body: const TabBarView(
+          children: [
+            OneDicePage(),
+            DicePage(),
+          ],
+        ),
       ),
-      body: DicePage(),
     ),
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int letfDiceNumber = 1;
+  int rightDiceNumber = 1;
+  bool _loading = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: TextButton(
-                onPressed: () {
-                  print('Left button got pressed');
-                },
-                child: Image.asset('images/dice1.png')),
-          ),
-          Expanded(
-            child: TextButton(
-                onPressed: () {
-                  print('Right button got pressed');
-                },
-                child: Image.asset('images/dice1.png')),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: _loading
+                    ? const CircularProgressIndicatorWidget()
+                    : Image.asset('images/dice$letfDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: _loading
+                    ? const CircularProgressIndicatorWidget()
+                    : Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 30.0,
+        ),
+        ElevatedButton.icon(
+          onPressed: () {
+            setState(() {
+              _loading = true;
+            });
+            Future.delayed(const Duration(milliseconds: 500)).then((_) {
+              throwDices();
+            });
+          },
+          icon: Image.asset('images/icons8-dice-100.png'),
+          label: const Text(''),
+          style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(Colors.red)),
+        )
+      ],
+    );
+  }
+
+  throwDices() {
+    setState(() {
+      _loading = false;
+      letfDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+}
+
+class OneDicePage extends StatefulWidget {
+  const OneDicePage({super.key});
+
+  @override
+  State<OneDicePage> createState() => _OneDicePageState();
+}
+
+class _OneDicePageState extends State<OneDicePage> {
+  int diceNumber = 1;
+  bool _loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _loading
+            ? const CircularProgressIndicatorWidget()
+            : Image.asset(
+                'images/dice$diceNumber.png',
+                height: 150.0,
+                width: 150.0,
+              ),
+        const SizedBox(height: 30.0),
+        ElevatedButton.icon(
+          onPressed: () {
+            setState(() {
+              _loading = true;
+            });
+            Future.delayed(const Duration(milliseconds: 500)).then((_) {
+              throwDice();
+            });
+          },
+          icon: Image.asset('images/icons8-dice-100.png'),
+          label: const Text(''),
+          style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(Colors.red)),
+        )
+      ],
+    );
+  }
+
+  throwDice() {
+    setState(() {
+      _loading = false;
+      diceNumber = Random().nextInt(6) + 1;
+    });
+  }
+}
+
+class CircularProgressIndicatorWidget extends StatelessWidget {
+  const CircularProgressIndicatorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator(
+      strokeWidth: 5,
+      backgroundColor: Colors.redAccent,
+      valueColor: AlwaysStoppedAnimation(Colors.red),
     );
   }
 }
